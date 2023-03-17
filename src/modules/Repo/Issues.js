1798567353a02f1f9302fb09f86b6d887fc5b148
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2023-03-17 16:10:14 +0100

    FetchIssues data provider

--- src/modules/Repo/Issues.js
+++ src/modules/Repo/Issues.js
@@ -1,19 +1,31 @@
 import {useCallback, useState} from 'react';
+import {useParams} from 'react-router-dom';
 import Component from '@src/components/Component';
 import './Issues.css';
-// import IssuesList from '@src/components/IssuesList';
-// import IssuesProvider from '@src/providers/FetchIssues';
+import FetchIssues from '@src/providers/FetchIssues';
+import {useFetchRepoContext} from '@src/providers/FetchRepo/ContextProvider';
 
 export default function Issues(props) {
+	const {data} = useFetchRepoContext();
+	const {name, owner} = useParams();
+
 	const [issues, setIssues] = useState(false);
 	const showIssues = useCallback(() => setIssues(true), []);
 	const hideIssues = useCallback(() => setIssues(false), []);
+
+	if (!data) {
+		return null;
+	}
 
 	return (
 		<Component className="issues" {...props}>
 			{!issues && <button type="button" onClick={showIssues}>Want to see issues?</button>}
 			{issues && <button type="button" onClick={hideIssues}>Hide them</button>}
-			{issues && <h1>Not implemented yet...</h1>}
+			{issues && (
+				<FetchIssues name={name} owner={owner}>
+					<h1>Not implemented yet...</h1>
+				</FetchIssues>
+			)}
 		</Component>
 	);
 }
