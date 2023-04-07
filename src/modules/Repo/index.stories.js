f07240b50acee207edaa8f4046814c0a3e845a87
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2023-04-07 13:05:48 +0200

    Interaction

--- src/modules/Repo/index.stories.js
+++ src/modules/Repo/index.stories.js
@@ -1,4 +1,6 @@
 import Repo from '.';
+import {expect} from '@storybook/jest';
+import {userEvent, within} from '@storybook/testing-library';
 import {query} from '@sb/msw';
 import ROUTES from '@src/ROUTES';
 import QueryFetchIssues from '@src/providers/FetchIssues/FetchIssues.gql';
@@ -10,16 +12,16 @@ export default {
 	component: Repo,
 	parameters: {
 		router: {route: ROUTES.repo, params: {name: 'repo-name', owner: 'owner-login'}},
+		msw: {handlers: {
+			QueryFetchRepo: query(QueryFetchRepo, REPO_RESPONSE),
+			QueryFetchIssues: query(QueryFetchIssues, ISSUES_RESPONSE),
+		}},
 	},
 };
 
 export function Fulfilled() {
 	return <Repo />;
 }
-Fulfilled.parameters = {msw: {handlers: {
-	QueryFetchRepo: query(QueryFetchRepo, REPO_RESPONSE),
-	QueryFetchIssues: query(QueryFetchIssues, ISSUES_RESPONSE),
-}}};
 
 export function SlowQuery() {
 	return <Repo />;
@@ -28,3 +30,15 @@ SlowQuery.parameters = {msw: {handlers: {
 	QueryFetchRepo: query(QueryFetchRepo, REPO_RESPONSE, {delay: 2000}),
 	QueryFetchIssues: query(QueryFetchIssues, ISSUES_RESPONSE, {delay: 2000}),
 }}};
+
+export function IssuesShown() {
+	return <Repo />;
+}
+IssuesShown.play = async ({canvasElement}) => {
+	const canvas = within(canvasElement);
+
+	const button = await canvas.findByRole('button', {name: 'Want to see issues?'});
+	userEvent.click(button);
+
+	await canvas.findByRole('list', {name: 'Issues list'});
+};
